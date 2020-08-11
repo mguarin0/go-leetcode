fd2dfc8e06edc76a18991ad0a82e28bba2dea386
mguarin0 <33071894+mguarin0@users.noreply.github.com>
2020-08-11 19:19:43 -0400

    Update two_sum.go

--- two_sum/two_sum.go
+++ two_sum/two_sum.go
@@ -1,13 +1,14 @@
 package two_sum
 
 func twoSum(nums []int, target int) []int {
-	m := make(map[int]int)
-	for i := 0; i < len(nums); i++ {
-		another := target - nums[i]
-		if _, ok := m[another]; ok {
-			return []int{m[another], i}
-		}
-		m[nums[i]] = i
-	}
-	return nil
+    m := make(map[int]int)
+    for i := 0; i < len(nums); i++ {
+        test := target - nums[i]
+        if _, found := m[test]; found {
+            return []int{m[test], i}
+        }
+        m[nums[i]] = i
+        
+    }
+    return nil    
 }
